fd8345b4f073dc61424ef50afba71a81a94bc40e
六翁 <lu.hl@alibaba-inc.com>
2021-08-14 16:57:20 +0800

    cpp setup

--- grpc/hello-grpc-cpp/client/proto_client.cpp
+++ grpc/hello-grpc-cpp/client/proto_client.cpp
@@ -1,32 +1,10 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 #include <iostream>
 #include <memory>
 #include <string>
 
 #include <grpcpp/grpcpp.h>
-
-#ifdef BAZEL_BUILD
-#include "examples/protos/helloworld.grpc.pb.h"
-#else
 #include "helloworld.grpc.pb.h"
-#endif
+#include "landing.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -34,75 +12,109 @@ using grpc::Status;
 using helloworld::Greeter;
 using helloworld::HelloReply;
 using helloworld::HelloRequest;
+using org::feuyeux::grpc::TalkRequest;
+using org::feuyeux::grpc::TalkResponse;
+using org::feuyeux::grpc::LandingService;
+using org::feuyeux::grpc::TalkResult;
+using org::feuyeux::grpc::ResultType;
 
-class GreeterClient {
- public:
-  GreeterClient(std::shared_ptr<Channel> channel)
-      : stub_(Greeter::NewStub(channel)) {}
+class LandingClient {
+public:
+    LandingClient(std::shared_ptr<Channel> channel) : client(LandingService::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SayHello(const std::string& user) {
-    // Data we are sending to the server.
-    HelloRequest request;
-    request.set_name(user);
+    void Talk() {
+        TalkRequest talkRequest;
+        std::string data("hello");
+        std::string meta("c++");
+        talkRequest.set_data(data);
+        talkRequest.set_meta(meta);
 
-    // Container for the data we expect from the server.
-    HelloReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->SayHello(&context, request, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
+        ClientContext context;
+        TalkResponse talkResponse;
+        Status status = client->Talk(&context, talkRequest, &talkResponse);
+        if (status.ok()) {
+            std::cout << "Talk status:" << talkResponse.status() << std::endl;
+            const TalkResult &talkResult = talkResponse.results(0);
+            ResultType resultType = talkResult.type();
+            std::cout << "Talk result:" << resultType << std::endl;
+        } else {
+            std::cout << "Talk error:" << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+        }
     }
-  }
 
- private:
-  std::unique_ptr<Greeter::Stub> stub_;
+private:
+    std::unique_ptr<LandingService::Stub> client;
 };
 
-int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint specified by
-  // the argument "--target=" which is the only expected argument.
-  // We indicate that the channel isn't authenticated (use of
-  // InsecureChannelCredentials()).
-  std::string target_str;
-  std::string arg_str("--target");
-  if (argc > 1) {
-    std::string arg_val = argv[1];
-    size_t start_pos = arg_val.find(arg_str);
-    if (start_pos != std::string::npos) {
-      start_pos += arg_str.size();
-      if (arg_val[start_pos] == '=') {
-        target_str = arg_val.substr(start_pos + 1);
-      } else {
-        std::cout << "The only correct argument syntax is --target="
-                  << std::endl;
-        return 0;
-      }
-    } else {
-      std::cout << "The only acceptable argument is --target=" << std::endl;
-      return 0;
-    }
-  } else {
-    target_str = "localhost:50051";
-  }
-  GreeterClient greeter(
-      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);
-  std::cout << "Greeter received: " << reply << std::endl;
+class GreeterClient {
+public:
+    GreeterClient(std::shared_ptr<Channel> channel) : stub_(Greeter::NewStub(channel)) {}
 
-  return 0;
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    std::string SayHello(const std::string &user) {
+        // Data we are sending to the server.
+        HelloRequest request;
+        request.set_name(user);
+
+        // Container for the data we expect from the server.
+        HelloReply reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        // The actual RPC.
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        // Act upon its status.
+        if (status.ok()) {
+            return reply.message();
+        } else {
+            std::cout << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char **argv) {
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint specified by
+    // the argument "--target=" which is the only expected argument.
+    // We indicate that the channel isn't authenticated (use of
+    // InsecureChannelCredentials()).
+    std::string target_str;
+    std::string arg_str("--target");
+    if (argc > 1) {
+        std::string arg_val = argv[1];
+        size_t start_pos = arg_val.find(arg_str);
+        if (start_pos != std::string::npos) {
+            start_pos += arg_str.size();
+            if (arg_val[start_pos] == '=') {
+                target_str = arg_val.substr(start_pos + 1);
+            } else {
+                std::cout << "The only correct argument syntax is --target="
+                          << std::endl;
+                return 0;
+            }
+        } else {
+            std::cout << "The only acceptable argument is --target=" << std::endl;
+            return 0;
+        }
+    } else {
+        target_str = "localhost:50051";
+    }
+    const std::shared_ptr<Channel> &channel = grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials());
+    GreeterClient greeter(channel);
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    std::cout << "Greeter received: " << reply << std::endl;
+    LandingClient landingClient(channel);
+    landingClient.Talk();
+    return 0;
 }
