5f4a943c3a1d02c3e1bf11fa5e147ab6bea05208
六翁 <lu.hl@alibaba-inc.com>
2021-08-25 16:55:45 +0800

    unify java-code-style

--- grpc/hello-grpc-java/src/main/java/org/feuyeux/grpc/HelloUtils.java
+++ grpc/hello-grpc-java/src/main/java/org/feuyeux/grpc/HelloUtils.java
@@ -1,21 +1,22 @@
 package org.feuyeux.grpc;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.toList;
-
 public class HelloUtils {
-    private static final Random random = new Random();
 
-    public static List<String> getRandomIds(int max) {
-        return IntStream.range(0, max)
-                .mapToObj(i -> getRandomId())
-                .collect(toList());
-    }
+  private static final Random random = new Random();
 
-    public static String getRandomId() {
-        return String.valueOf(random.nextInt(5));
-    }
+  public static List<String> getRandomIds(int max) {
+    return IntStream.range(0, max)
+        .mapToObj(i -> getRandomId())
+        .collect(toList());
+  }
+
+  public static String getRandomId() {
+    return String.valueOf(random.nextInt(5));
+  }
 }
--- grpc/hello-grpc-java/src/test/java/org/feuyeux/grpc/ProtoTest.java
+++ grpc/hello-grpc-java/src/test/java/org/feuyeux/grpc/ProtoTest.java
@@ -1,5 +1,11 @@
 package org.feuyeux.grpc;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import lombok.extern.slf4j.Slf4j;
 import org.feuyeux.grpc.client.ProtoClient;
 import org.feuyeux.grpc.proto.TalkRequest;
@@ -11,67 +17,60 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
-import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
-
 /**
- * Created by erichan feuyeux
- * on 16/8/22
+ * Created by erichan feuyeux on 16/8/22
  */
 @Slf4j
 public class ProtoTest {
-    public static String getLocalIp() {
-        try {
-            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-            InetAddress ip;
-            while (allNetInterfaces.hasMoreElements()) {
-                NetworkInterface netInterface = allNetInterfaces.nextElement();
-                if ("lo".equals(netInterface.getName())) {
-                    // 如果是回环网卡跳过
-                    continue;
-                }
-                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
-                while (addresses.hasMoreElements()) {
-                    ip = addresses.nextElement();
-                    if (ip instanceof Inet4Address) {
-                        String t = ip.getHostAddress();
-                        if (!"127.0.0.1".equals(t)) {
-                            // 只返回不是本地的IP
-                            return t;
-                        }
-                    }
-                }
-            }
-            return null;
-        } catch (SocketException e) {
-            log.error("", e);
-            return null;
+
+  @Rule
+  public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
+  public static String getLocalIp() {
+    try {
+      Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+      InetAddress ip;
+      while (allNetInterfaces.hasMoreElements()) {
+        NetworkInterface netInterface = allNetInterfaces.nextElement();
+        if ("lo".equals(netInterface.getName())) {
+          // 如果是回环网卡跳过
+          continue;
         }
+        Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+        while (addresses.hasMoreElements()) {
+          ip = addresses.nextElement();
+          if (ip instanceof Inet4Address) {
+            String t = ip.getHostAddress();
+            if (!"127.0.0.1".equals(t)) {
+              // 只返回不是本地的IP
+              return t;
+            }
+          }
+        }
+      }
+      return null;
+    } catch (SocketException e) {
+      log.error("", e);
+      return null;
     }
+  }
 
-    @Rule
-    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+  @Test()
+  public void testProto() throws InterruptedException, IOException {
+    environmentVariables.set("GRPC_HELLO_SECURE", "Y");
+    ProtoServer protoServer = new ProtoServer(new LandingServiceImpl());
+    ProtoClient protoClient = new ProtoClient();
+    TalkRequest talkRequest = TalkRequest.newBuilder()
+        .setMeta("id=" + System.nanoTime())
+        .setData("eric")
+        .build();
+    log.info("REQUEST:{}", talkRequest);
+    TalkResponse talkResponse = protoClient.talk(talkRequest);
 
-    @Test()
-    public void testProto() throws InterruptedException, IOException {
-        environmentVariables.set("GRPC_HELLO_SECURE", "Y");
-        ProtoServer protoServer = new ProtoServer(new LandingServiceImpl());
-        ProtoClient protoClient = new ProtoClient();
-        TalkRequest talkRequest = TalkRequest.newBuilder()
-                .setMeta("id=" + System.nanoTime())
-                .setData("eric")
-                .build();
-        log.info("REQUEST:{}", talkRequest);
-        TalkResponse talkResponse = protoClient.talk(talkRequest);
+    Assert.assertTrue(talkResponse.getStatus() == 200);
+    log.info("RESPONSE:{}", talkResponse);
 
-        Assert.assertTrue(talkResponse.getStatus() == 200);
-        log.info("RESPONSE:{}", talkResponse);
-
-        protoClient.shutdown();
-        protoServer.stop();
-    }
+    protoClient.shutdown();
+    protoServer.stop();
+  }
 }
